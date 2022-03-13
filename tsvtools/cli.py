4f6da103282e02ebf07304ae87d0a382ff9b0299
thamnasc <thalitanas09@gmail.com>
2022-03-13 15:35:57 -0300

    option --segment-type was removed

--- tsvtools/cli.py
+++ tsvtools/cli.py
@@ -84,7 +84,6 @@ def annotate_tsv(tsv_file, annotated_tsv_file):
 @click.option('--min-confidence', type=float, default=None)
 @click.option('--max-confidence', type=float, default=None)
 @click.option('--ned-priority', type=int, default=1)
-@click.option('--segment-type', type=str, default='Page')
 # def page2tsv(page_xml_file, tsv_out_file, purpose, image_url, ner_rest_endpoint, ned_rest_endpoint,
 def page2tsv(mets_file, tsv_out_file, file_grp, purpose, image_url, ner_rest_endpoint, ned_rest_endpoint,
              noproxy, scale_factor, ned_threshold, min_confidence, max_confidence, ned_priority, scheme, server,
